1b6e1b4cacdb3fe718ee7300fa04192d3e3309eb
root <root@ip-172-31-0-191.ap-south-1.compute.internal>
2022-11-03 09:25:32 +0000

    modified

--- provider.tf
+++ provider.tf
@@ -1,5 +1,15 @@
 provider "aws" {
-    access_key = "access_key   "
-    secret_key = "secret_key"
+    access_key = "AKIA5FIOCDDBUQG2PD5K"
+    secret_key = "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"
     region = "ap-south-1"	
+}
+terraform {
+  backend "s3" {
+    bucket         = "mybucketforterra"
+    key            = "ec2rdss3/terraform.tfstate"
+    region         = "ap-south-1"
+
+    dynamodb_table = "dynamodb-lock"
+    encrypt        = true
+  }
 }
